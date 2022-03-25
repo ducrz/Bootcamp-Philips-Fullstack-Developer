7f6a0066d24b04c60513143f1efb0b3ce4cbb144
ducrz <ducatolico@gmail.com>
2022-03-25 17:59:46 -0300

    Fim Curso Módulos Javascript.

---
+++ javascript/trabalhando-com-modulos-em-javascript/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}.`;
+}
+
+function mostraCidade(nome, cidade) {
+    return `A idade de ${nome} é ${cidade}.`;
+}
+
+function mostraHobby(nome, hobby) {
+    return `A idade de ${nome} é ${hobby}!`;
+}
+
+export {
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+}
---
+++ javascript/trabalhando-com-modulos-em-javascript/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Eduardo', 32));
+
+console.log(mostraCidade('Eduardo', 'São Paulo'));
+
+console.log(mostraHobby('Eduardo', 'Automóveis'));

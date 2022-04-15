14839def334b92ed9572d407536cc60703426403
ducrz <ducatolico@gmail.com>
2022-04-15 19:00:13 -0300

    Fim do Curso: Entendendo Métodos Java.

---
+++ Java/EntendendoMetodosJava/MetodosJava/src/com/dio/quadrilateros/Main.java
@@ -0,0 +1,15 @@
+package com.dio.quadrilateros;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
+

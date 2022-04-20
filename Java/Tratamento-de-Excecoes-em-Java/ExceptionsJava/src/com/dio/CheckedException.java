69cf44a13a96609e378443477701b02f9a04b029
ducrz <ducatolico@gmail.com>
2022-04-20 14:16:24 -0300

    Fim Curso: Tratamento de Exceções em Java.

---
+++ Java/Tratamento-de-Excecoes-em-Java/ExceptionsJava/src/com/dio/CheckedException.java
@@ -0,0 +1,44 @@
+package com.dio;
+
+
+import javax.swing.*;
+import java.io.*;
+
+public class CheckedException {
+    public static void main(String[] args) {
+        String nomeDoArquivo = "testedearquivo.txt";
+
+        try {
+            imprimirArquivoNoConsole(nomeDoArquivo);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null,
+                    "Revise o nome do arquivo que você deseja imprimir! " + e.getCause());
+            e.printStackTrace();
+        } catch (IOException e){
+            //e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                    "Ocorreu um erro inesperado! Entre em contato com o suporte! " + e.getCause());
+        } finally {
+            System.out.println("Chegou no finally!");
+        }
+
+        System.out.println("Apesar da exception ou não, o programa continua...");
+    }
+
+    public static void imprimirArquivoNoConsole(String nomeDoArquivo) throws IOException {
+        File file = new File(nomeDoArquivo);
+
+        BufferedReader br = new BufferedReader(new FileReader(file.getName()));
+        String line = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        do{
+            bw.write(line);
+            bw.newLine();
+            line=br.readLine();
+        } while(line != null);
+        bw.flush();
+        br.close();
+    }
+}
---
+++ Java/Tratamento-de-Excecoes-em-Java/ExceptionsJava/src/com/dio/DivisaoNaoExataException.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public class DivisaoNaoExataException extends Exception{
+
+    private int numerado;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerado, int denominador) {
+        super(message);
+        this.numerado = numerado;
+        this.denominador = denominador;
+    }
+}

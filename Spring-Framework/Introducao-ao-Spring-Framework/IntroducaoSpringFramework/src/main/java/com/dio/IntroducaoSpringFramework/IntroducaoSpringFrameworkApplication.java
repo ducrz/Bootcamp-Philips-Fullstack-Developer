c9b1a1abcaea48b27a2cb44e71f55c4dd339f20f
ducrz <ducatolico@gmail.com>
2022-04-21 13:45:49 -0300

    Fim Curso: Introdução ao Spring Framework.

---
+++ Spring-Framework/Introducao-ao-Spring-Framework/IntroducaoSpringFramework/src/main/java/com/dio/IntroducaoSpringFramework/IntroducaoSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.dio.IntroducaoSpringFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ Spring-Framework/Introducao-ao-Spring-Framework/IntroducaoSpringFramework/src/test/java/com/dio/IntroducaoSpringFramework/IntroducaoSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.IntroducaoSpringFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroducaoSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
